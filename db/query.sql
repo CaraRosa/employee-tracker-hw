90882a2d7d9637b254e07e77f5ba63528eafa040
Cara R <cara.rosa.02@gmail.com>
2023-10-31 21:56:55 -0500

    added code to display table correctly when viewing all employees

---
+++ db/query.sql
@@ -0,0 +1,12 @@
+SELECT
+    e.first_name,
+    e.last_name,
+    r.title AS role,
+    d.department_name AS department,
+    r.salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+
+    FROM employee e
+    LEFT JOIN role r ON e.role_id = r.id
+    LEFT JOIN department d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id;
